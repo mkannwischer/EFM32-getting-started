1ef0bff28dd3f6a8bfdba3077cfc025ca4b49af0
Matthias J. Kannwischer <matthias@kannwischer.eu>
2020-10-01 15:45:17 +0200

    add AES CTR

--- hal-aes.h
+++ hal-aes.h
@@ -7,7 +7,8 @@
 void hal_aes128_ecb(uint8_t *out, const uint8_t *in, size_t nblocks, const uint8_t *key);
 void hal_aes256_ecb(uint8_t *out, const uint8_t *in, size_t nblocks, const uint8_t *key);
 
-void hal_aes128_ctr(uint8_t *out, size_t outlen, uint8_t *nonce, const uint8_t *key);
-void hal_aes256_ctr(uint8_t *out, size_t outlen, uint8_t *nonce, const uint8_t *key);
+
+void hal_aes128_ctr(uint8_t *out, size_t outlen, const uint8_t *iv, const uint8_t *key);
+void hal_aes256_ctr(uint8_t *out, size_t outlen, const uint8_t *iv, const uint8_t *key);
 
 #endif
